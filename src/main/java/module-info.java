d42c87a57c598ed92909e76e4fd0f0b1cd77981e
parsa khoshnoud <pskh6126@gmail.com>
2024-07-08 10:45:00 +0330

    Login completed

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.hotelmanagmentsystem {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens org.example.hotelmanagmentsystem to javafx.fxml;
-    exports org.example.hotelmanagmentsystem;
-}
